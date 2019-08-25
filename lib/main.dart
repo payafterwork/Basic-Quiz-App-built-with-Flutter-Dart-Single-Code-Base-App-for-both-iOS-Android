853399688584d64f9d20d94fe435dd5d08a7fc51
Harshit Batra <payafterwork.in@gmail.com>
2019-08-25 11:09:02 +0530

    Stateful widget and setState() to rerender widget that changed

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,23 @@ void main(){
  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget{ //Changing state class widget
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+var questionIndex = 0;
+class MyAppState extends State<MyApp>{  //Persistant state class widget 
  Widget build(BuildContext context){
+
+  void answerQuestion(){
+    setState((){
+    questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
   @override
    var questions =  ['What\'s are your favourite color?', 'What\'s are your favourite animal?' ];
     return MaterialApp(
@@ -14,10 +29,12 @@ class MyApp extends StatelessWidget{
          ), 
           body: Column(
             children: [
-            RaisedButton(child: Text('Answer 1'),onPressed: null),
-            RaisedButton(child: Text('Answer 2'),onPressed: null),
-            RaisedButton(child: Text('Answer 3'),onPressed: null),
-            ],
+            Text(questions.elementAt(questionIndex)),  
+            RaisedButton(child: Text('Answer 1'),onPressed: answerQuestion), //Passed just pointer to function for later execution
+            RaisedButton(child: Text('Anonymous function call'),onPressed: ()=>{
+              print('Anonymous function called')
+            }), // Running anonymous function
+         ],
        )
        )
     );
