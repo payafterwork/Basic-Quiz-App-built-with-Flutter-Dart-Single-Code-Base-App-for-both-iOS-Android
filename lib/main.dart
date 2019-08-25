8cacb50c538a63da0cb247ded7fed2b92975e98c
Harshit Batra <payafterwork.in@gmail.com>
2019-08-25 10:04:58 +0530

    Basic Flutter App Bar + Text Output

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+void main(){
+ runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+ Widget build(BuildContext context){
+    return MaterialApp(
+       home: Scaffold(
+         appBar: AppBar(
+           title: Text('My First App'),
+         ), 
+          body: Text('This is default text!')
+       )
+    );
+  }
+}
+   
